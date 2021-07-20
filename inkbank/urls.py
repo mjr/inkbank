32cf3cef8a84254d4126ffc887c9aa54e21a16e6
Manaia Junior <manaiajr.23@gmail.com>
2021-07-20 15:54:02 -0300

    Add home page

--- inkbank/urls.py
+++ inkbank/urls.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic.base import RedirectView
 
+from inkbank.core.views import index
+
+
 urlpatterns = [
+    path("", index, name="index"),
     path("restricted/", admin.site.urls),
     path("admin/", RedirectView.as_view(url="https://www.djangoproject.com")),
 ]
