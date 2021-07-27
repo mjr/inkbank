e7bf16887cfe0917318d00282625c8d51387095b
Manaia Junior <manaiajr.23@gmail.com>
2021-07-27 12:46:37 -0300

    Add bonus account

--- inkbank/accounts/forms.py
+++ inkbank/accounts/forms.py
@@ -7,10 +7,15 @@ from .models import Account
 class NewAccountForm(forms.ModelForm):
     class Meta:
         model = Account
-        fields = ["number"]
+        fields = ["kind", "number"]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.fields["kind"].widget.attrs.update(
+            {
+                "class": "mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            }
+        )
         self.fields["number"].widget.attrs.update(
             {
                 "class": "focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-md sm:text-sm border-gray-300"
